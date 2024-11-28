ed2b69480249611e4a3ae358acc558e44b5131c6
diegoglez <diegoglezh98@gmail.com>
2024-11-28 17:18:43 +0200

    Creacion e implementacion de pagina de servicios

--- src/components/servicios/servicios.js
+++ src/components/servicios/servicios.js
@@ -80,7 +80,7 @@ class Servicios extends React.Component {
                       <p className="card-text">
                         This is a wider card with supporting text below as a
                         natural lead-in to additional content. This content is a
-                        little bit longer.
+                        little bit longer .
                       </p>
                       <div className="d-flex justify-content-between align-items-center">
                         <div className="btn-group">
